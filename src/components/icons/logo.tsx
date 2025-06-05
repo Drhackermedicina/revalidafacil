86c065a777d2eff9bcdbd575192764a40293b0a9
hellitoncechinel1 cechinel <hellitoncechinel1@gmail.com>
2025-06-05 07:14:35 +0000

    QUERO ASSIM O ICONE

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -9,7 +9,7 @@ interface LogoProps extends Omit<SVGProps<SVGSVGElement>, 'children'> {
 const Logo = ({ width, height, className, ...rest }: LogoProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 24 24" // Standard viewBox
     fill="currentColor"
     width={width || 40} // Default width if not provided
     height={height || 40} // Default height if not provided
@@ -18,7 +18,12 @@ const Logo = ({ width, height, className, ...rest }: LogoProps) => (
     {...rest}
   >
     <title>Revalida Fácil Logo - Capelo de Formando</title>
-    <path d="M12 3L1 9L12 15L23 9L12 3ZM5 10.47V17L12 21L19 17V10.47L12 14.27L5 10.47Z" />
+    {/* Mortarboard (Top Diamond) */}
+    <path d="M12 2 L22 7 L12 12 L2 7 Z" />
+    {/* Cap Body (Trapezoid that fits under the diamond) */}
+    <path d="M5 10 L19 10 L17 17 L7 17 Z" />
+    {/* Tassel (hanging left from top-center of mortarboard) */}
+    <path d="M12 2 L12 6 L9 6 L9 13 Z" />
   </svg>
 );
 
