87927b8c681050bd0cf8f24913a7f79b56fb5f06
hellitoncechinel1 cechinel <hellitoncechinel1@gmail.com>
2025-06-05 08:55:56 +0000

    quero que preencha o fundo da pagina com o mesmo degrade de cores da aba

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { SidebarNav } from "@/components/layout/sidebar-nav";
@@ -12,7 +13,10 @@ export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex min-h-screen bg-background">
       <SidebarNav />
-      <div className="flex-1 flex flex-col md:ml-0 transition-all duration-300 ease-in-out"> {/* Adjust ml if sidebar is collapsed fixed width */}
+      <div className={cn(
+        "flex-1 flex flex-col md:ml-0 transition-all duration-300 ease-in-out",
+        "bg-gradient-to-r from-primary to-[hsl(var(--chart-2))] text-primary-foreground"
+      )}>
         <HeaderNav />
         <main className="flex-1 p-4 md:p-6 lg:p-8">
           {children}
